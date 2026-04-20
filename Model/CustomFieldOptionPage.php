80e5a85459ff79d50f3817ab5e6e2f6b2d5d0d60
Pierre Baguet <pbaguet@openstudio.fr>
2026-04-20 15:45:15 +0200

    add option pages management with CRUD functionality

---
+++ Model/CustomFieldOptionPage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CustomFields\Model;
+
+use CustomFields\Model\Base\CustomFieldOptionPage as BaseCustomFieldOptionPage;
+
+class CustomFieldOptionPage extends BaseCustomFieldOptionPage
+{
+
+}
